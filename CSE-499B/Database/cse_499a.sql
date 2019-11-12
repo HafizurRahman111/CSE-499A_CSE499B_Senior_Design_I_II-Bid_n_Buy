6186d9f080aa937c7ea58210ccc8dda68a152452
HafizurRahman111 <rahman.hafizur@northsouth.edu>
2019-11-12 19:57:44 +0600

    Updated Database File Uploaded with new tables

--- CSE-499B/Database/cse_499a.sql
+++ CSE-499B/Database/cse_499a.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2019 at 08:21 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.7
+-- Generation Time: Nov 12, 2019 at 02:53 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,14 +40,6 @@ CREATE TABLE `buyer_profile` (
   `email` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `buyer_profile`
---
-
-INSERT INTO `buyer_profile` (`bp_id`, `b_id`, `username`, `works`, `current_address`, `parmanent_address`, `birthday`, `language`, `email`) VALUES
-(4, 0, 'himel', '', '', '', '0000-00-00 00:00:00.000000', '', ''),
-(5, 0, 'barua', 'CSE', 'MIRPUR2', 'TANGAIL1', '2019-10-01 00:00:00.000000', 'Bengali', 'himel@gmail.com');
-
 -- --------------------------------------------------------
 
 --
@@ -56,21 +48,60 @@ INSERT INTO `buyer_profile` (`bp_id`, `b_id`, `username`, `works`, `current_addr
 
 CREATE TABLE `buyer_signup` (
   `b_id` int(20) NOT NULL,
-  `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(20) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `phone` varchar(20) NOT NULL,
+  `firstname` varchar(25) NOT NULL,
+  `lastname` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `phone` varchar(25) NOT NULL,
   `password` varchar(30) NOT NULL,
   `reg_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `buyer_signup`
+-- Table structure for table `buy_products_demand`
 --
 
-INSERT INTO `buyer_signup` (`b_id`, `firstname`, `lastname`, `username`, `phone`, `password`, `reg_time`) VALUES
-(75, 'Hafizur', 'Rahman', 'himel', '01680735420', 'null', '2019-10-23 05:59:44'),
-(77, 'Himel', 'Himel', 'barua', '01965165175', 'sala', '2019-10-23 06:00:24');
+CREATE TABLE `buy_products_demand` (
+  `b_product_id` int(20) NOT NULL,
+  `buyer_username` varchar(25) NOT NULL,
+  `buyer_name` varchar(25) NOT NULL,
+  `buyer_email` varchar(25) NOT NULL,
+  `buyer_phone` varchar(25) NOT NULL,
+  `buyer_category` varchar(15) NOT NULL,
+  `buyer_type` varchar(15) NOT NULL,
+  `product_name` varchar(25) NOT NULL,
+  `estimated_price` double(10,2) NOT NULL,
+  `quantity` varchar(10) NOT NULL,
+  `product_details` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `b_products_images`
+--
+
+CREATE TABLE `b_products_images` (
+  `b_pro_image_id` int(20) NOT NULL,
+  `product_id` int(20) NOT NULL,
+  `filename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `contact_id` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `subject` varchar(20) NOT NULL,
+  `message` varchar(110) NOT NULL,
+  `m_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -104,15 +135,6 @@ CREATE TABLE `seller_signup` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `seller_signup`
---
-
-INSERT INTO `seller_signup` (`s_id`, `firstname`, `lastname`, `username`, `phone`, `password`, `reg_time`) VALUES
-(39, 'afrin', 'afrin', 'afrin', '12345', 'sam', '2019-10-22 23:34:09'),
-(42, 'Ashikur', 'Rahman', 'ashik', '988898989892', 'ashik', '2019-10-22 23:34:39'),
-(46, 'Saddat', 'Hasan', 'badhan', '2563773993', 'badhan', '2019-10-22 23:36:15');
-
---
 -- Indexes for dumped tables
 --
 
@@ -130,6 +152,26 @@ ALTER TABLE `buyer_signup`
   ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`phone`),
   ADD UNIQUE KEY `b_id` (`b_id`);
+
+--
+-- Indexes for table `buy_products_demand`
+--
+ALTER TABLE `buy_products_demand`
+  ADD PRIMARY KEY (`b_product_id`),
+  ADD UNIQUE KEY `buyer_username` (`buyer_username`);
+
+--
+-- Indexes for table `b_products_images`
+--
+ALTER TABLE `b_products_images`
+  ADD PRIMARY KEY (`b_pro_image_id`),
+  ADD UNIQUE KEY `b_pro_image_id` (`b_pro_image_id`);
+
+--
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`contact_id`);
 
 --
 -- Indexes for table `seller_profile`
@@ -152,13 +194,25 @@ ALTER TABLE `seller_signup`
 -- AUTO_INCREMENT for table `buyer_profile`
 --
 ALTER TABLE `buyer_profile`
-  MODIFY `bp_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `bp_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `buyer_signup`
 --
 ALTER TABLE `buyer_signup`
-  MODIFY `b_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `b_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT for table `buy_products_demand`
+--
+ALTER TABLE `buy_products_demand`
+  MODIFY `b_product_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `contact_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `seller_profile`
