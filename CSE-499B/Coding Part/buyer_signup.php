563815aeb1da8c190e9c666938b53142c88a0303
HafizurRahman111 <rahman.hafizur@northsouth.edu>
2019-11-15 21:42:25 +0600

    Buyer Singup Page Complete With Updated database table

--- CSE-499B/Coding Part/buyer_signup.php
+++ CSE-499B/Coding Part/buyer_signup.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -11,6 +12,7 @@
   <meta name="Himel" content="" />
 
   <!-- css -->
+
   <link href="https://fonts.googleapis.com/css?family=Handlee|Open+Sans:300,400,600,700,800" rel="stylesheet">
   <link href="css/bootstrap.css" rel="stylesheet" />
   <link href="css/bootstrap-responsive.css" rel="stylesheet" />
@@ -21,6 +23,7 @@
   <link href="css/style.css" rel="stylesheet" />
 
   <!-- google font -->
+
   <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -31,6 +34,7 @@
 
 
   <!-- Theme skin -->
+
   <link href="color/default.css" rel="stylesheet" />
 
   <title>Buyer Signup</title>
@@ -86,26 +90,32 @@
 
      <div id="wrapper">
 
-    <!-- start header -->
+    <!---------------- Start Header -------------->
+
     <header>
+
       <div class="top">
         <div class="container">
           <div class="row">
+
             <div class="span6">
               <p class="topcontact"><i class="icon-phone"></i> +880 1675695322</p>
             </div>
+
             <div class="span6">
 
               <ul class="social-network">
+
                 <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-facebook icon-white"></i></a></li>
                 <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-twitter icon-white"></i></a></li>
                 <li><a href="#" data-placement="bottom" title="Linkedin"><i class="icon-linkedin icon-white"></i></a></li>
                 <li><a href="#" data-placement="bottom" title="Pinterest"><i class="icon-pinterest  icon-white"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Google +"><i class="icon-google-plus icon-white"></i></a></li>
+                
                 
               </ul>
 
             </div>
+
           </div>
         </div>
       </div>
@@ -113,46 +123,47 @@
 
       <div class="container">
 
-
          <div class="row nomargin">
            <div class="span3">
+
              <div class="logo" id="logo">
                <a href="index.php"><img src="img/logo.jpg" alt="" />Buy & Sell</a>
              </div>
+
            </div>
+
            <div class="span9">
              <div class="navbar navbar-static-top">
                <div class="navigation">
                  <nav>
+
                    <ul class="nav topnav">
+
                      <li class="active">
-                       <a href="index.php">Home
-                       </a>
+                       <a href="index.php">Home</a>
                      </li>
 
                      <li>
-                      <a href="buyProduct.php">Buy Products</a>
-                    </li>
+                       <a href="buyProduct.php">Buy Products</a>
+                     </li>
 
-                    <li>
-                      <a href="sellProduct.php">Sell Products</a>
-                    </li>
+                     <li>
+                       <a href="sellProduct.php">Sell Products</a>
+                     </li>
 
-                    <li>
-                      <a href="contactUs.php">Contact Us</a>
-                    </li>
+                     <li>
+                       <a href="contactUs.php">Contact Us</a>
+                     </li>
 
-
-                    <li>
-                      <a href="aboutUs.php">About Us</a>
-                    </li>
-
+                     <li>
+                       <a href="aboutUs.php">About Us</a>
+                     </li>
 
 
                     <li>
                     <a href="#myModal" role="button" class="btn btn-large btn-primary" data-toggle="modal">Sign Up</a>
 
-                    <!-- popup for login -->
+                    <!-------------- Popup for Sign Up --------------->
 
                     <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
 
@@ -183,6 +194,7 @@
                                     </div>
 
                                   </form>
+
                       </div>
 
 
@@ -191,13 +203,12 @@
                   </li>
 				  
 				  
-				
 				  
 			    <li>	  
 				 
-				  <!--Log In Pop Up--> 
+				  <!---------------  Log In Pop Up  ---------------> 
 				  
-                    <a class="btn btn-large btn-primary" role="button" href="#" data-target="#login" data-toggle="modal">Log In</a>
+            <a class="btn btn-large btn-primary" role="button" href="#" data-target="#login" data-toggle="modal">Log In</a>
 
                        <div id="login" class="modal fade" role="dialog">
                           <div class="modal-dialog">
@@ -229,7 +240,7 @@
 													  
 													  <button class="btn btn-secondary" data-dismiss="modal">Close</button>
 													  
-                                                    </div>														                                       										         
+                                            </div>														                                       										         
 													  
                                                 </div>
  
@@ -247,25 +258,23 @@
 						
 			    </li>	  
 				  
-				  
-				  
-				  
-				  
-				  
-				  
-				  
-				  
+				
 
                   </ul>
                 </nav>
+
               </div>
-              <!-- end navigation -->
+
+              <!-------------------   End Navigation   -------------------->
+
             </div>
           </div>
         </div>
       </div>
+
     </header>
-    <!-- end header -->
+
+      <!--------------------------   End Header  ---------------------->
 
 
 
@@ -274,64 +283,61 @@
   ?>
 
 
-     <!-- Registration Form -->
+     <!------------------   Registration Form  ------------------->
 
-<section id="buyersignup"> 
+   <section id="buyersignup"> 
   
 	  <div class="c11 container">
-        <div class="row">
+      <div class="row">
 
         <div class="card" id="Registration">
-          <h5 class="card-header" class="py-5 text-center font-weight-bold">Sign Up as a Buyer</h5>
+          <h5 class="card-header" class="py-5 text-center font-weight-bold">Sign Up : <b>Buyer</b></h5>
 
-		 <div class="row" class="card-body">
+		       <div class="row" class="card-body">
 
+             <div class="col-md-8 box-1">
 
+               <form method="post" enctype="multipart/form-data">
 
-           <div class="col-md-8 box-1">
-              <form method="post" enctype="multipart/form-data">
-                 <div class="text">
+                 <div class="text"></div>
 
-                </div>
+                  <div class="form-group">
+                   <input type="text" required class="form-control" placeholder="First Name" name="fname">
+                  </div>
 
+                  <div class="form-group">
+                   <input type="text" required class="form-control" placeholder="Last Name" name="lname">
+                  </div>
 
-
-
-              <div class="form-group">
-                   <input type="text" class="form-control" id="exampleInputPassword1" placeholder="First Name" name="fname">
-               </div>
-               <div class="form-group">
-                   <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last Name" name="lname">
-               </div>
-               <div class="form-group">
-                   <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Username" name="uname">
-               </div>
+                  <div class="form-group">
+                   <input type="text" required class="form-control" placeholder="Username" name="uname">
+                  </div>
  
-               <div class="form-group">
-                   <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter Phone Number" name="phone">
-               </div>
+                  <div class="form-group">
+                    <input type="text" required class="form-control" placeholder="Enter Phone Number" name="phone">
+                  </div>
 
-               <div class="form-group">
-                   <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="pass">
-               </div>
-               
+                  <div class="form-group">
+                    <input type="password" required class="form-control" placeholder="Password" name="pass">
+                  </div>
 
+                  <br>
 
-               <br>
-                   <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 
-				</form>
-
-
-         <!--PHP Code Started for Client Registration -->
+				        </form>
 
 
-        <?php
+     <!------------- PHP Code Started for Buyer Registration ---------------->
+
+
+    <?php
         
-          include_once("connection.php");
+        include_once("connection.php");
 
-           if(isset($_POST['submit']))
-      {
+
+         if(isset($_POST['submit']))
+       {
 
           $fname = $_POST['fname'];
           $lname = $_POST['lname'];
@@ -339,15 +345,11 @@
           $phone = $_POST['phone'];
           $pass = $_POST['pass'];
 
-         
-          $reg_time = date("Y-m-d H:i:s");
 
-          $sql = "INSERT INTO buyer_signup(firstname,lastname,username,phone,password,reg_time)
-                  VALUES('$fname','$lname','$uname','$phone','$pass','$reg_time')";
+          $buyer_signup_table = "INSERT INTO buyer_signup(firstname,lastname,username,phone,password)
+                  VALUES('$fname','$lname','$uname','$phone','$pass')";
 
           
-
-
                  if(empty($fname) || empty($lname) || empty($uname) || empty($pass) || empty($phone) )
                 {
 
@@ -360,10 +362,10 @@
                       });</script>";
                   }
 
-                   elseif(!$res = mysqli_query($conn,$sql))
+                   elseif(!$res = mysqli_query($conn,$buyer_signup_table))
                   {
                      echo"<script>swal({
-                      title: 'Enter Another Username,Password',
+                      title: 'Error Occured. Please Try again.',
                       text: 'Thank You',
                       icon: 'error',
                       timer: 4000,
@@ -375,15 +377,17 @@
                    else
                   {
 
-                    $res = mysqli_query($conn,$sql);
+                    $res = mysqli_query($conn,$buyer_signup_table);
                     echo"<script>swal({
                       title: 'Congratulation! Your Information Added Successfully',
                       text: 'Registration Complete as a Buyer.Thank You',
                       icon: 'success',
-                      timer: 4000,
+                      timer: 6000,
                       button: false,
-                      });</script>";
+                      });
 
+                      window.location='index.php'</script>";
+                      
 
                    $lastInsertId = $conn->insert_id;
 
@@ -403,31 +407,30 @@
 
                 }
 
-        ?>
+    ?>
 
 
 
+              </div>
 
-           </div>
+            </div>
+          </div>
 
+      </div>
 
-           </div>
-
-
-         </div>
-
-
- </div>
-         </section>
+   </section>
 
         
+  <!-----------------------  Footer Part Start------------------>
 
 
-      <footer>                                      <!-- Footer part start-->
+      <footer>
+
         <div class="container">
           <div class="row">
             <div class="span4">
               <div class="widget">
+
                 <h5 class="widgetheading">Browse pages</h5>
 
                 <ul class="link-list">
@@ -442,32 +445,43 @@
 
             <div class="span4">
               <div class="widget">
+
                 <h5 class="widgetheading">Get in touch</h5>
+
                 <address>
                 <strong>Buy & Sell</strong><br>
                 Bashundhara Residential Area<br>
                 Dhaka, Bangladesh
-              </address>
+               </address>
+
                 <p>
                   <i class="icon-phone"></i> +880 1675695322 <br>
                   <i class="icon-envelope-alt"></i> buynsell@gmail.com
                 </p>
+
               </div>
             </div>
+
+
             <div class="span4">
               <div class="widget">
+
                 <h5 class="widgetheading">Subscribe for latest product updates</h5>
+
                 <p>
                   Keep track for new products Sell & Buy updates. Enter your e-mail and subscribe to our newsletter.
                 </p>
+
                 <form class="subscribe">
                   <div class="input-append">
                     <input class="span2" id="appendedInputButton" type="text">
                     <button class="btn btn-theme" type="submit">Subscribe</button>
                   </div>
                 </form>
+
               </div>
             </div>
+
           </div>
         </div>
 
@@ -475,6 +489,7 @@
           <div class="container">
             <div class="row">
               <div class="span6">
+
                 <div class="copyright">
                   <p><span>&copy; Company. All right reserved</span></p>
                 </div>
@@ -482,24 +497,27 @@
               </div>
 
               <div class="span6">
+
                 <div class="credits">
                   Designed for CSE499</a>
                 </div>
+
               </div>
             </div>
           </div>
         </div>
 
       </footer>
-</div>                              <!-- Footer part end-->
+    </div>                             
 
 
+   <!---------------------  Footer Part End  ------------------->
 
-<a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bglight icon-2x active"></i></a>
 
-<!-- javascript
-   ================================================== -->
- <!-- Placed at the end of the document so the pages load faster -->
+ <a href="#" class="scrollup"><i class="icon-angle-up icon-square icon-bglight icon-2x active"></i></a>
+
+  <!--------------------------    javascript    --------------------->
+
  <script src="js/jquery.js"></script>
  <script src="js/jquery.easing.1.3.js"></script>
  <script src="js/bootstrap.js"></script>
@@ -520,7 +538,12 @@
  <script src="js/inview.js"></script>
 
  <!-- Template Custom JavaScript File -->
+
  <script src="js/custom.js"></script>
 
-</body>
-</html>
+
+  </body>
+
+ </html>
+
+
