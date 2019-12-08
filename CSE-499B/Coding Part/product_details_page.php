4c2bd7d4e520f85c7c50adc0786d33ecb7c561c5
HafizurRahman111 <rahman.hafizur@northsouth.edu>
2019-12-08 09:18:28 +0600

    Product Details Page Updated

--- CSE-499B/Coding Part/product_details_page.php
+++ CSE-499B/Coding Part/product_details_page.php
@@ -348,17 +348,7 @@
           </div>
           
           <div class="container service1-items col-sm-2 col-md-2 pull-left">
-            <center>
-              <a id="item-1" class="service1-item">
-                <img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-              </a>
-              <a id="item-2" class="service1-item">
-                <img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-              </a>
-              <a id="item-3" class="service1-item">
-                <img src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png" alt=""></img>
-              </a>
-            </center>
+           
           </div>
 
         </div>
@@ -379,33 +369,45 @@
    $sql = "SELECT * FROM buy_products_demand  LIMIT 1";
 
    
-
      if ($result = mysqli_query($conn,$sql))
    {
           
         while ($row = mysqli_fetch_assoc($result)) 
        {
           
+            echo "  <div class='product-title'><h2><b>Product Name : ".$row['product_name']."</b></h2></div>
+                     
+                       <p>".$row['buyer_category']."</p>
+                       <br>
 
-            echo "  <div class='product-title'>Product Name : ".$row['product_name']."</div>
-                     <br>
+                       <h6>Product ID : ".$row['b_product_id']."</h6>  
+                       <h6> Type : <b> <font color='#FF1493'> ".$row['buyer_type']." </font></b></h6>  
+                       <h6>Quantity : ".$row['quantity']."</h6>
 
-                      <div class='product-desc'>Product Details : ".$row['product_details']."</div>
-                      
+                       
+                       <hr>
+                       <div class='product-price'><h3>Price : <b><font color='#1E90FF'> ".$row['estimated_price']."</font></b></h3></div>
 
+                       <div class='product-stock'><b><font color='#00FF00'>".$row['product_status']."</font></div>
+                       </hr>
 
+                       
+                        <br>
+                        <hr>
 
-                        
+                        <div class='btn-group cart'>
+                        <button type='button' class='btn btn-success'> Add to cart </button>
+                        </div>
 
-                       <h5><b>".$row['buyer_category']."</b></h5>
+                        <div class='btn-group wishlist'>
+                        <button type='button' class='btn btn-danger'>Add to wishlist </button>
+                        </div>
+                    
 
-                                <p>Product ID : ".$row['b_product_id']."</p>
-                                
-                                <p>Price : ".$row['estimated_price']."</p>
-                                <p>Quantity : ".$row['quantity']."</p>
+                        <br>
+                        <br>
+                        <br>
 
-                              
-                                <p><button class='btn btn-success btn-sm'><a href='product_details_page.php' >".$row['buyer_type']."</a></button></p>
 
                       </div>
                     </div>
@@ -417,100 +419,104 @@
                }
    
 
-
       }    
-
-
-
 
       
 ?>
 
 
      <!--------------------------    PHP  ------------------------------>
-
-
-
-
-
-
-
-
-
-
-
+    
 
 
 
 
           
-          
-          <div class="product-rating"><i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star gold"></i> <i class="fa fa-star-o"></i> </div>
-          <hr>
-          <div class="product-price">$ 1234.00</div>
-          <div class="product-stock">In Stock</div>
-          <hr>
-          <div class="btn-group cart">
-            <button type="button" class="btn btn-success">
-              Add to cart 
-            </button>
-          </div>
-          <div class="btn-group wishlist">
-            <button type="button" class="btn btn-danger">
-              Add to wishlist 
-            </button>
-          </div>
-        </div>
+         
+         
 
 
-      </div> 
+    </div> 
     </div>
     <div class="container-fluid">   
       <div class="col-md-12 product-info">
           <ul id="myTab" class="nav nav-tabs nav_tabs">
-            
-            <li class="active"><a href="#service-one" data-toggle="tab">DESCRIPTION</a></li>
-            <li><a href="#service-two" data-toggle="tab">PRODUCT INFO</a></li>
-            <li><a href="#service-three" data-toggle="tab">REVIEWS</a></li>
-            
-          </ul>
-        <div id="myTabContent" class="tab-content">
-            <div class="tab-pane fade in active" id="service-one">
-             
-              <section class="container product-info">
-                The Corsair Gaming Series GS600 power supply is the ideal price-performance solution for building or upgrading a Gaming PC. A single +12V rail provides up to 48A of reliable, continuous power for multi-core gaming PCs with multiple graphics cards. The ultra-quiet, dual ball-bearing fan automatically adjusts its speed according to temperature, so it will never intrude on your music and games. Blue LEDs bathe the transparent fan blades in a cool glow. Not feeling blue? You can turn off the lighting with the press of a button.
 
-                <h3>Corsair Gaming Series GS600 Features:</h3>
-                <li>It supports the latest ATX12V v2.3 standard and is backward compatible with ATX12V 2.2 and ATX12V 2.01 systems</li>
-                <li>An ultra-quiet 140mm double ball-bearing fan delivers great airflow at an very low noise level by varying fan speed in response to temperature</li>
-                <li>80Plus certified to deliver 80% efficiency or higher at normal load conditions (20% to 100% load)</li>
-                <li>0.99 Active Power Factor Correction provides clean and reliable power</li>
-                <li>Universal AC input from 90~264V — no more hassle of flipping that tiny red switch to select the voltage input!</li>
-                <li>Extra long fully-sleeved cables support full tower chassis</li>
-                <li>A three year warranty and lifetime access to Corsair’s legendary technical support and customer service</li>
-                <li>Over Current/Voltage/Power Protection, Under Voltage Protection and Short Circuit Protection provide complete component safety</li>
-                <li>Dimensions: 150mm(W) x 86mm(H) x 160mm(L)</li>
-                <li>MTBF: 100,000 hours</li>
-                <li>Safety Approvals: UL, CUL, CE, CB, FCC Class B, TÜV, CCC, C-tick</li>
+
+
+
+
+    
+  <?php 
+      
+
+  
+   include 'connection.php';
+   
+   $conn = mysqli_connect("localhost","root","","cse_499a"); 
+   
+   $sql = "SELECT * FROM buy_products_demand  LIMIT 1";
+
+   
+     if ($result = mysqli_query($conn,$sql))
+   {
+          
+        while ($row = mysqli_fetch_assoc($result)) 
+       {
+          
+            echo "  
+              <li class='active'><a href='#service-one' data-toggle='tab'>DESCRIPTION</a></li>
+            <li><a href='#service-two' data-toggle='tab'>REVIEWS & COMMENTS</a></li>
+            
+            </ul>
+            <div id='myTabContent' class='tab-content'>
+            <div class='tab-pane fade in active' id='service-one'>
+             
+              <section class='container product-info'> <div class='product-desc'><h4><b>Details : </b></h4><br><br>".$row['product_details']."</div>
+                
+                <h3>  </h3>
+                <li>   </li>
+                <li>    </li>
+
               </section>
                       
             </div>
-          <div class="tab-pane fade" id="service-two">
+
+            <div class='tab-pane fade' id='service-two'>
             
-            <section class="container">
+            <section class='container'>
                 
             </section>
             
-          </div>
-          <div class="tab-pane fade" id="service-three">
+           </div>
+
+           <div class='tab-pane fade' id='service-three'>
                         
           </div>
+          
         </div>
         <hr>
       </div>
     </div>
   </div>
-</div>
+</div> " ;
+
+
+               }
+   
+
+      }    
+
+      
+?>
+
+
+     <!--------------------------    PHP  ------------------------------>
+    
+
+
+
+
 
 
 
